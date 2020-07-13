8cabc02910d7f98b44e38e7dc56287b1571857d2
harshithpabbati <pabbatiharshith@gmail.com>
2020-07-13 19:34:26 +0530

    add verify email API

--- framework/schema.py
+++ framework/schema.py
@@ -5,6 +5,14 @@ from graphene_django import DjangoObjectType
 from django.contrib.auth.models import User
 from framework.api.queries.user import UserBasicObj
 import blog.schema
+from django.core.mail import send_mail
+from framework import settings
+from django.template.loader import render_to_string
+from django.utils.html import strip_tags
+import hashlib
+
+secret_key = settings.SECRET_KEY
+from_email = settings.EMAIL_HOST_USER
 
 
 class UserType(DjangoObjectType):
@@ -15,6 +23,26 @@ class UserType(DjangoObjectType):
 
 class userStatusObj(graphene.ObjectType):
     status = graphene.Boolean()
+
+
+class VerifyOTP(graphene.Mutation):
+    class Arguments:
+        otp = graphene.String(required=True)
+
+    Output = userStatusObj
+
+    def mutate(self, info, otp):
+        user = User.objects.values().get(username=info.context.user)
+        s = secret_key + user['email']
+        hashEncoded = hashlib.md5(s.encode())
+        encodedHash = hashEncoded.hexdigest()[:4]
+        if encodedHash == otp:
+            user.is_active = True
+            user.save()
+            status = True
+        else:
+            status = False
+        return userStatusObj(status=status)
 
 
 class CreateUser(graphene.Mutation):
@@ -33,6 +61,16 @@ class CreateUser(graphene.Mutation):
         )
         newUser.set_password(password)
         newUser.save()
+        s = secret_key + email
+        hashEncoded = hashlib.md5(s.encode())
+        otp = hashEncoded.hexdigest()[:4]
+        context = {
+            "otp": otp,
+            "username": username
+        }
+        message = render_to_string('email/verify_email.html', context)
+        send_mail('Verification | Micro Blogging', strip_tags(message), from_email, [email], fail_silently=False,
+                  html_message=message)
 
         return userStatusObj(status=True)
 
@@ -61,6 +99,7 @@ class Mutation(blog.schema.Mutation, graphene.ObjectType):
     refresh_token = graphql_jwt.Refresh.Field()
     revoke_token = graphql_jwt.Revoke.Field()
     create_user = CreateUser.Field()
+    verify_otp = VerifyOTP.Field()
 
 
 schema = graphene.Schema(query=Query, mutation=Mutation)
